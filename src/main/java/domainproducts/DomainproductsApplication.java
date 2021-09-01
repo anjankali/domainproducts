fbf8fd64c987ab609b9828209e3497a68f851795
Anjan Kali <anjan.kali@gmail.com>
2021-09-01 14:54:27 +0530

    Inital Commit

---
+++ src/main/java/domainproducts/DomainproductsApplication.java
@@ -0,0 +1,13 @@
+package domainproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/domainproducts/DomainproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package domainproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
